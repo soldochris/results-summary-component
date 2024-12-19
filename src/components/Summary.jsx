a830b1f30395e4c0335b7433f99e5871a85e43ac
Christian Soldevilla <soldevilla.christian@gmail.com>
2024-12-18 20:32:56 -0500

    summary ready for mobile

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -1,19 +1,45 @@
+import './summary.css'
+
 function Summary(){
   return(
-    <section>
+    <section className="summary">
         <h2>Summary</h2>
         <ul>
           <li>
-            <span>Reaction</span> <span>80 / 100</span>
+            <span className='category'> 
+              <img src="/assets/images/icon-reaction.svg" alt="icon" /> 
+              Reaction
+            </span>
+            <div>
+              80 <span>/ 100</span>
+            </div>
           </li>
           <li>
-            <span>Memory</span> <span>92 / 100</span>
+            <span className='category'>
+              <img src="/assets/images/icon-memory.svg" alt="icon" /> 
+              Memory
+            </span>
+            <div>
+              92 <span>/ 100</span>
+            </div>
           </li>
           <li>
-            <span>Verbal</span> <span>61 / 100</span>
+            <span className='category'>
+              <img src="/assets/images/icon-verbal.svg" alt="icon" /> 
+              Verbal
+            </span> 
+            <div>
+              61 <span>/ 100</span>
+            </div>
           </li>
           <li>
-            <span>Visual</span> <span>72 / 100</span>
+            <span className='category'>
+              <img src="/assets/images/icon-visual.svg" alt="icon" /> 
+              Visual
+            </span> 
+            <div>
+              72 <span> / 100</span>
+            </div>
           </li>
         </ul>
         <button>Continue</button>
