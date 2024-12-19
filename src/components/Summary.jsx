2531cd347bb8c1caa736247770b1996fe0bbb33c
Christian Soldevilla <soldevilla.christian@gmail.com>
2024-12-19 00:05:14 -0500

    hotfix

--- src/components/Summary.jsx
+++ src/components/Summary.jsx
@@ -9,37 +9,37 @@ function Summary({data}){
           <li>
             <span className='category'> 
               <img src={data[0]?.icon} alt="icon" /> 
-              {data[0].category}
+              {data[0]?.category}
             </span>
             <div>
-              {data[0].score} <span>/ 100</span>
+              {data[0]?.score} <span>/ 100</span>
             </div>
           </li>
           <li>
             <span className='category'>
               <img src={data[1]?.icon} alt="icon" /> 
-              {data[1].category}
+              {data[1]?.category}
             </span>
             <div>
-            {data[1].score} <span>/ 100</span>
+            {data[1]?.score} <span>/ 100</span>
             </div>
           </li>
           <li>
             <span className='category'>
               <img src={data[2]?.icon} alt="icon" /> 
-              {data[2].category}
+              {data[2]?.category}
             </span> 
             <div>
-            {data[2].score} <span>/ 100</span>
+            {data[2]?.score} <span>/ 100</span>
             </div>
           </li>
           <li>
             <span className='category'>
               <img src={data[3]?.icon} alt="icon" /> 
-              {data[3].category}
+              {data[3]?.category}
             </span> 
             <div>
-            {data[3].score} <span> / 100</span>
+            {data[3]?.score} <span> / 100</span>
             </div>
           </li>
         </ul>
